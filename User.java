edbfb59cd9d1ff2b169cb8a81b7f44a0d91c15f9
kathe <katherinebaik05@gmail.com>
2025-06-16 14:43:23 -0700

    commit from intelliJ

--- User.java
+++ User.java
@@ -2,6 +2,10 @@ public class User {
     private int userID;
     private String username;
     private String password;
-    private int role;
+    private Role role;
 
+
+    enum Role{
+        BUYER, SELLER, ADMIN
+    }
 }
