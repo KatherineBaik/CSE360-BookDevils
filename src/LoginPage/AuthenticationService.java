baeff8409b2415d381a1f0c50a50952fc9f9efed
sini1234 <56083162+sini1234@users.noreply.github.com>
2025-06-20 10:36:30 -0700

    AuthenticationService.java

--- src/LoginPage/AuthenticationService.java
+++ src/LoginPage/AuthenticationService.java
@@ -1,27 +1,29 @@
 package LoginPage;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AuthenticationService {
 
-    static Map<String, UserInfo> users = new HashMap<>();
+    static List<UserInfo> users = new ArrayList<>();
 
     static class UserInfo {
+        String asuId;
         String password;
         String role;
 
-        UserInfo(String password, String role) {
+        UserInfo(String asuId, String password, String role) {
+            this.asuId = asuId;
             this.password = password;
             this.role = role;
         }
     }
 
     static void initializeUsers() {
-        users.put("1234567890", new UserInfo("password123", "Buyer"));
-        users.put("0987654321", new UserInfo("admin123", "Admin"));
-        users.put("1122334455", new UserInfo("seller123", "Seller"));
-        users.put("2233445566", new UserInfo("buyer123", "Buyer"));
+        users.add(new UserInfo("1234567890", "password123", "Buyer"));
+        users.add(new UserInfo("0987654321", "admin123", "Admin"));
+        users.add(new UserInfo("1122334455", "seller123", "Seller"));
+        users.add(new UserInfo("2233445566", "buyer123", "Buyer"));
     }
 
     static boolean authenticate(String asuId, String password, String role) {
@@ -33,20 +35,27 @@ public class AuthenticationService {
             return false;
         }
 
-        UserInfo user = users.get(asuId);
-
-        if (user == null) {
-            return false;
+        for (UserInfo user : users) {
+            if (user.asuId.equals(asuId) && 
+                user.password.equals(password) && 
+                user.role.equals(role)) {
+                return true;
+            }
         }
 
-        return user.password.equals(password) && user.role.equals(role);
+        return false;
     }
 
     static void addUser(String asuId, String password, String role) {
-        users.put(asuId, new UserInfo(password, role));
+        users.add(new UserInfo(asuId, password, role));
     }
 
     static boolean userExists(String asuId) {
-        return users.containsKey(asuId);
+        for (UserInfo user : users) {
+            if (user.asuId.equals(asuId)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
